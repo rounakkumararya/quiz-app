b6e69f48bb7501c3118b88fd3c0f8deb8880b06f
rounakkumararya <rounakkumar040@gmail.com>
2025-02-21 10:22:43 +0530

    improve design and fix errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from 'react'
 import Quiz from './Quiz';
+import { FaAngleDown } from 'react-icons/fa';
 
 const Hero = () => {
     const [quizStarted, setQuizStarted] = useState(false);
@@ -14,9 +15,32 @@ const Hero = () => {
                 <div className="flex flex-col w-full   gap-5 items-center text-center p-5  ">
 
                     <h1 className='text-6xl font-semibold'>Welcome to the Quiz Platform...</h1>
+                    <div className="m-2 max-w-[80%] ">
+                        <div
+                            className="group flex flex-col gap-2 rounded-lg bg-gray-50 p-4 text-black"
+                            tabIndex="1"
+                        >
+                            <div className="flex cursor-pointer items-center justify-between">
+                                <span className='text-md font-semibold'> Please read the instructions below </span>
+                                <FaAngleDown />
+                            </div>
+                            <div
+                                className="invisible h-auto max-h-0 items-center opacity-0 transition-all group-focus:visible group-focus:max-h-screen group-focus:opacity-100 group-focus:duration-1000"
+                            >
+                                <ol className='list-disc font-extralight text-sm flex flex-col gap-2 text-start'>
+                                    <li> For multiple-choice questions, select the one best answer (A, B, C, or D)</li>
+                                    <li>For integer-type questions, write your numerical answer clearly.</li>
+                                    <li>Each questions have a time limit of 30 seconds</li>
+                                </ol>
+                            </div>
+                        </div>
+
+                    </div>
                     <h2 className='text-xl'>Please click the button below to start the quiz</h2>
                     <button className="bg-violet-600 max-w-[200px] hover:bg-violet-800  duration-500 text-white font-bold py-2 px-8 rounded " onClick={() => setQuizStarted(true)}>Start Quiz</button>
                 </div>
+
+
 
             </div>
             ) : (
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -8,7 +8,7 @@ const Quiz = () => {
     const [score, setScore] = React.useState(0);
     const [integerAnswers, setIntegerAnswers] = React.useState("");
     const [timeLeft, setTimeLeft] = React.useState(30);
-    const [seletedAnswer, setSelectedAnswer] = React.useState("");
+    const [selectedAnswer, setSelectedAnswer] = React.useState("");
     const [showCorrectAnswer, setShowCorrectAnswer] = React.useState(false);
 
     useEffect(() => {
@@ -21,7 +21,18 @@ const Quiz = () => {
         return () => clearInterval(timer);
     }, [timeLeft]);
 
+
     const handleNextQuestion = () => {
+        if (questions[currentQuestion].type === "Integer") {
+            if (parseInt(integerAnswers) === parseInt(questions[currentQuestion].correctAnswer)) {
+                console.log("correct");
+                setScore(score + 1);
+                setSelectedAnswer(integerAnswers);
+
+
+            }
+        }
+
         if (currentQuestion < questions.length - 1) {
 
             setCurrentQuestion(currentQuestion + 1);
@@ -34,27 +45,23 @@ const Quiz = () => {
         }
     };
 
+
     const handleAnswer = (selectedAnswer) => {
         if (questions[currentQuestion].type === "Integer") {
-
-            if (parseInt(integerAnswers) === parseInt(questions[currentQuestion].correctAnswer)) {
-
-                setScore(score + 1);
-                setSelectedAnswer(integerAnswers);
-
-            }
-
-
-        } else {
-
-            if (selectedAnswer === questions[currentQuestion].correctAnswer) {
-                setScore(score + 1);
-                setSelectedAnswer(selectedAnswer);
-
-            }
+            setIntegerAnswers(selectedAnswer);
         }
 
+
+        if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+            console.log("correct");
+            setScore(score + 1);
+            setSelectedAnswer(selectedAnswer);
+
+        }
+
+
         setShowCorrectAnswer(true);
+
 
 
     };
@@ -92,16 +99,19 @@ const Quiz = () => {
                                 borderColor:
                                     showCorrectAnswer && option === questions[currentQuestion].correctAnswer
                                         ? "lightgreen"
-                                        : showCorrectAnswer && seletedAnswer !== option
+                                        : showCorrectAnswer && selectedAnswer !== option
                                             ? "lightcoral"
                                             : "",
 
+
                                 cursor: showCorrectAnswer ? "default" : "pointer",
-                            }} onClick={() => handleAnswer(option)} className=" has-checked:bg-gray-300 active:bg-gray-200 max-w-[90%] list-none border-2 cursor-pointer border border-gray-400 hover:bg-gray-200 rounded p-2 px-4" key={index} >{option}</li>
+                            }} onClick={() => handleAnswer(option)} className=" has-checked:bg-gray-300 active:bg-gray-200 max-w-[90%] list-none border-2 focus cursor-pointer  border-gray-400 hover:bg-gray-200 rounded p-2 px-4" key={index} >{option}</li>
                         ))) : (
+                            <div className="">
 
-                            <input style={{ borderColor: showCorrectAnswer && seletedAnswer !== questions[currentQuestion].correctAnswer ? "red" : "green" }} type="number" className='p-2 border border-gray-300 rounded active:outline-none' placeholder='Enter your answer in number ' onChange={(e) => setIntegerAnswers(e.target.value)} value={integerAnswers} />
-
+                                <input style={{ borderColor: integerAnswers ? (parseInt(integerAnswers) !== questions[currentQuestion].correctAnswer ? "red" : "lightgreen") : "" }} type="number" className='p-2 border-2 focus:border-0 focus:outline-none focus:ring-0 hover:border-gray-400 border-gray-300 rounded ' placeholder='Enter your answer in number ' onChange={(e) => setIntegerAnswers(e.target.value)} value={integerAnswers} />
+                                {console.log(parseInt(integerAnswers) === questions[currentQuestion].correctAnswer)}
+                            </div>
 
                         )
 
@@ -110,6 +120,7 @@ const Quiz = () => {
                         }
                         <button type='submit' onClick={handleNextQuestion} className=' bg-emerald-400  max-w-[200px] hover:bg-emerald-800  duration-500 text-white font-bold py-2 px-8 rounded '>{currentQuestion === questions.length - 1 ? "Submit" : "Next"}</button>
                     </div>
+
                     <div className="">{currentQuestion + 1} / {questions.length}</div>
                 </div>
             )
